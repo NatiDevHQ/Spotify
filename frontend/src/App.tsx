984607cd65ef631fec1e78d00c4e0634127b7c3d
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-07-18 08:10:54 -0400

    add clerk provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,23 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <header>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
+    </div>
+  );
 };
 
 export default App;
