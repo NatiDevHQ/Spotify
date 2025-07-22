668b6cd123c4f4862aa52f40e41d99a3071984fd
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-07-22 09:37:43 -0400

    add the error handler and admin for uploading img and audio file ...

--- backend/src/controller/admin.controller.js
+++ backend/src/controller/admin.controller.js
@@ -1,3 +1,40 @@
-export const createSong = (req, res) => {
-  res.send("Admin route is working");
+import { Song } from "../models/song.model.js";
+import { Album } from "../models/album.model.js";
+
+export const createSong = async (req, res, next) => {
+  try {
+    if (!req.files || !req.files.audioFile || !req.files.imageFile) {
+      return res.status(400).json({
+        message: "please upload all files",
+      });
+    }
+
+    const { title, artist, albumId, duration } = req.body;
+    const audioFile = req.files.audioFile;
+    const imageFile = req.files.imageFile;
+
+    const audioUrl = await uploadToCloudinary(audioFile);
+    const imageUrl = await uploadToCloudinary(imageFile);
+
+    const song = new Song({
+      title,
+      artist,
+      audioUrl,
+      imageUrl,
+      duration,
+      albumId: albumId || null,
+    });
+    await song.save();
+
+    //if song belong to an album, update the albums song array
+    if (albumId) {
+      await Album.findByIdAndUpdate(albumId, {
+        $push: { songs: song_id },
+      });
+    }
+    res.status(201).json(song);
+  } catch (error) {
+    console.log("error in creating song", error);
+    next(error);
+  }
 };
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { clerkMiddleware } from "@clerk/express";
+import fileupload from "express-fileupload";
+import path from "path";
 import connectDB from "./lib/db.js";
 import userRouter from "./routes/user.route.js";
 import adminRouter from "./routes/admin.route.js";
@@ -9,13 +11,23 @@ import songRouter from "./routes/song.route.js";
 import albumRouter from "./routes/album.route.js";
 import statRouter from "./routes/stat.route.js";
 import authRouter from "./routes/auth.route.js";
-
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 const PORT = process.env.PORT;
 app.use(express.json());
 app.use(clerkMiddleware()); //this will add auth to req
+app.use(
+  fileupload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, "tmp"),
+    createParentPath: true,
+    limits: {
+      fileSize: 10 * 1024 * 1024, //max 10mg
+    },
+  })
+);
 
 app.use("/api/users", userRouter);
 app.use("/api/auth", authRouter);
@@ -23,6 +35,16 @@ app.use("/api/admin", adminRouter);
 app.use("/api/songs", songRouter);
 app.use("/api/albums", albumRouter);
 app.use("/api/stats", statRouter);
+
+//error handler
+app.use((error, req, res, next) => {
+  res.status(500).json({
+    message:
+      process.env.NODE_ENV === "production"
+        ? "internal server error"
+        : error.message,
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
