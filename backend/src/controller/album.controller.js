4f44e24043e4b13be486e199ef73e8bf8c826c74
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-07-23 03:33:05 -0400

    correct error  on the album page

--- backend/src/controller/album.controller.js
+++ backend/src/controller/album.controller.js
@@ -12,12 +12,13 @@ export const getAllAlbums = async (req, res, next) => {
 export const getAlbumById = async (req, res, next) => {
   try {
     const { albumId } = req.params;
+
     const album = await Album.findById(albumId).populate("songs");
+
     if (!album) {
-      return res.status(404).json({
-        message: "album not found",
-      });
+      return res.status(404).json({ message: "Album not found" });
     }
+
     res.status(200).json(album);
   } catch (error) {
     next(error);
