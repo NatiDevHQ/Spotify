e0921fbfef11c84da9eaab34529e4e9bfdf69149
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-07-23 04:15:44 -0400

    complete the stats route now everthing is working as expected

--- backend/src/routes/stat.route.js
+++ backend/src/routes/stat.route.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
+import { getStats } from "../controller/stat.controller.js";
 
 const router = Router();
-router.get("/");
+
+router.get("/", protectRoute, requireAdmin, getStats);
+
 export default router;
