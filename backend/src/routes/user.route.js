e45ca67eafa77d1cb5e1e981653bbf9476aae94b
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-07-23 04:00:24 -0400

    conplete the user routes

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { protectRoute } from "../middleware/auth.middleware";
+import { getAllUsers } from "../controller/user.controller";
 
 const router = Router();
 router.get("/", protectRoute, getAllUsers);
