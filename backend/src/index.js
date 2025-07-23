ef0ff8b1a48c6d34cd09e1ab826f893d7d33c6dc
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-07-23 04:16:52 -0400

    conplete the BE no to the FE

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
 import { clerkMiddleware } from "@clerk/express";
 import fileupload from "express-fileupload";
