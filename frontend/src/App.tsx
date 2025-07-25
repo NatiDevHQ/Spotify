6c9cd771972aa243ebf3c22e500aed0107985968
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-07-25 08:19:08 -0400

    set up for tthe front complete now to the auth page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,14 @@
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  UserButton,
-} from "@clerk/clerk-react";
-import { Button } from "./components/ui/button";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import AuthCallbackPage from "./pages/auth/AuthCallbackPage";
 
 const App = () => {
   return (
     <div>
-      <header>
-        <SignedOut>
-          <SignInButton>
-            <Button>sign in</Button>
-          </SignInButton>
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </header>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth-callback" element={<AuthCallbackPage />} />
+      </Routes>
     </div>
   );
 };
